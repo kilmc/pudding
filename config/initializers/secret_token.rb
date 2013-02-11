e62f039b6f2b35342b3ac2473f78287c35937340
Kilian McMahon <kil@kilmc.com>
2013-02-11 21:38:31 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pudding::Application.config.secret_token = '6d40b3b22bde2b3eec3efe501a0d3b782166a9b19b63735b79356feeb06ab5adab0d1f33ed9d9c82334ecb3e707db39a972a4cd3dca3d119b5b3241c239630de'
