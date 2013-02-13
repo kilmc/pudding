c1c1a4b8ccc929940718e3bbc3d752da7b5f847d
Kilian McMahon <kil@kilmc.com>
2013-02-13 22:16:50 +0100

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+describe "Static page" do
+
+  describe "Home page" do
+    
+    it "should have the h1 'Pudding'" do
+
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Home')
+    end
+
+    it "should have the h1 'Pudding'" do
+
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Pudding | Home")
+    end
+  end
+
+  describe "Help page" do
+    
+    it "should have the h1 'Help'" do
+
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the right title" do
+
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Pudding | Help")
+    end
+  end
+
+  describe "About page" do
+    
+    it "should have the h1 'About'" do
+
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About')
+    end
+
+    it "should have the right title" do
+
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Pudding | About" )
+    end
+  end
+
+  describe "Contact page" do
+    
+    it "should have the h1 'Contact'" do
+
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the right title" do
+
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Pudding | Contact" )
+    end
+  end
+end
